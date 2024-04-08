5fb24d3da327629c4f0f4098f917cea992152300
anettehoglund <hoglundanette@gmail.com>
2024-04-08 17:16:50 +0200

    js update

---
+++ src/index.js
@@ -0,0 +1,43 @@
+function updateTime(){
+  let stockholm = document.querySelector("#stockholm");
+  let stockholmDate = stockholm.querySelector(".date");
+  let stockholmTime = stockholm.querySelector(".time");
+
+  stockholmDate.innerHTML = moment().format("MMMM Do, YYYY");
+  stockholmTime.innerHTML = moment()
+    .tz("Europe/Stockholm")
+    .format("hh:mm:ss [<bold>]A[</bold>]");
+
+
+  let london = document.querySelector("#london");
+  let londonDate = london.querySelector(".date");
+  let londonTime = london.querySelector(".time");
+
+  londonDate.innerHTML = moment().format("MMMM Do, YYYY");
+  londonTime.innerHTML = moment()
+    .tz("Europe/London")
+    .format("hh:mm:ss [<bold>]A[</bold>]");
+
+
+  let newyork = document.querySelector("#newyork");
+  let newyorkDate = newyork.querySelector(".date");
+  let newyorkTime = newyork.querySelector(".time");
+
+  newyorkDate.innerHTML = moment().format("MMMM Do, YYYY");
+  newyorkTime.innerHTML = moment()
+    .tz("America/New_York")
+    .format("hh:mm:ss [<bold>]A[</bold>]");
+
+ 
+  let iceland = document.querySelector("#iceland");
+  let icelandDate = iceland.querySelector(".date");
+  let icelandTime = iceland.querySelector(".time");
+
+  icelandDate.innerHTML = moment().format("MMMM Do, YYYY");
+  icelandTime.innerHTML = moment()
+    .tz("Atlantic/Reykjavik")
+    .format("hh:mm:ss [<bold>]A[</bold>]");
+}
+
+  updateTime();
+  setInterval(updateTime, 1000);
