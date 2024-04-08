2eb8c831285ae711c5e0d4c75ec92d6874614bdc
anettehoglund <hoglundanette@gmail.com>
2024-04-08 19:12:14 +0200

    final

--- src/index.js
+++ src/index.js
@@ -55,7 +55,8 @@ function updateTime(){
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
             </div>
             <div class="time">${cityTime.format("h:mm:ss ")}<bold>${cityTime.format("A")}</bold></div>
-        </div>`;
+        </div>
+        <a href="index.html" class="back">🔙</a>`;
   }
 
   let chooseCityHere = document.querySelector("#chooseCity");
